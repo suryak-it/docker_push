5c77778e5e9ac27c2db8d451db274640bc200302
surya <suryak.22it@kongu.edu>
2025-03-19 16:52:32 +0530

    folder change

--- New_docker_push/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Variables
-IMAGE_NAME="suryak542005/dockerhub"
-TAG="latest"
-
-# Build Docker image
-docker build -t $IMAGE_NAME:$TAG .
-echo "Docker image $IMAGE_NAME:$TAG built successfully."
